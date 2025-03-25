aea99473bb40574081bf3cdda6c961f6a4263284
Ameya Lad <22u1741@students.git.edu>
2025-03-25 13:24:39 +0530

    updated the files

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
 			<div className='container mx-auto px-4 py-3'>
 				<div className='flex flex-wrap justify-between items-center'>
 			     	<Link to='/' className='text-2xl font-bold text-emerald-400 flex items-center space-x-2'>
-                       <img src='Kiw.png' alt='Logo' className='h-8 w-8' />
+                       <img src='Kiw.png' alt='Logo' className='h-9 w-9' />
                       <span>KIW</span>
                      </Link>
 
